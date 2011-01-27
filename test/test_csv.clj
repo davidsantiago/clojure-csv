dc178f175c6b54900255b81044e0a5efc5a1011b
David Santiago <david.santiago@gmail.com>
2011-01-27 19:35:42 +0100

    Untabify test file.

--- test/test_csv.clj
+++ test/test_csv.clj
@@ -1,6 +1,6 @@
 (ns test-csv
   (:use clojure.test
-	clojure-csv.core))
+        clojure-csv.core))
 
 (deftest basic-functionality
   (is (= [["a" "b" "c"]] (parse-csv "a,b,c")))
@@ -12,29 +12,29 @@
   (is (= [["Before", "", "After"]] (parse-csv "Before,\"\",After")))
   (is (= [["", "start&end", ""]] (parse-csv "\"\",\"start&end\",\"\"")))
   (is (= [[",", "\"", ",,", ",,,"]]
-	 (parse-csv "\",\",\"\"\"\",\",,\",\",,,\"")))
+         (parse-csv "\",\",\"\"\"\",\",,\",\",,,\"")))
   (is (= [["quoted", "\",\"", "comma"]]
-	 (parse-csv "quoted,\"\"\",\"\"\",comma"))))
+         (parse-csv "quoted,\"\"\",\"\"\",comma"))))
 
 (deftest newlines
   (is (= [["test1","test2"] ["test3","test4"]]
-	 (parse-csv "test1,test2\ntest3,test4")))
+         (parse-csv "test1,test2\ntest3,test4")))
   (is (= [["test1","test2"] ["test3","test4"]]
-	 (parse-csv "test1,test2\r\ntest3,test4")))
+         (parse-csv "test1,test2\r\ntest3,test4")))
   (is (= [["embedded","line\nbreak"]] (parse-csv "embedded,\"line\nbreak\"")))
   (is (= [["embedded", "line\r\nbreak"]]
-	 (parse-csv "embedded,\"line\r\nbreak\""))))
+         (parse-csv "embedded,\"line\r\nbreak\""))))
 
 (deftest writing
   (is (= "test1,test2\n" (write-csv [["test1" "test2"]])))
   (is (= "test1,test2\ntest3,test4\n"
-	 (write-csv [["test1" "test2"] ["test3" "test4"]])))
+         (write-csv [["test1" "test2"] ["test3" "test4"]])))
   (is (= "quoted:,\"carriage\nreturn\"\n"
-	 (write-csv [["quoted:" "carriage\nreturn"]])))
+         (write-csv [["quoted:" "carriage\nreturn"]])))
   (is (= "quoted:,\"embedded,comma\"\n"
-	 (write-csv [["quoted:" "embedded,comma"]])))
+         (write-csv [["quoted:" "embedded,comma"]])))
   (is (= "quoted:,\"escaped\"\"quotes\"\"\"\n"
-	 (write-csv [["quoted:" "escaped\"quotes\""]]))))
+         (write-csv [["quoted:" "escaped\"quotes\""]]))))
 
 (deftest nonstring-inputs
   (is (= [["First", "Second"]] (parse-csv (seq "First,Second"))))
@@ -42,7 +42,7 @@
 
 (deftest alternate-delimiters
   (is (= [["First", "Second"]]
-	 (binding [*delimiter* \tab] (parse-csv "First\tSecond")))))
+         (binding [*delimiter* \tab] (parse-csv "First\tSecond")))))
 
 (deftest strictness
   ;; I can't figure out why, but the thrown? tests always fail, even though
@@ -50,11 +50,11 @@
   ;(is (thrown? Exception (binding [*strict* true] (parse-csv "a,b,c,\"d"))))
   ;(is (thrown? Exception (binding [*strict* true] (parse-csv "a,b,c,d\"e"))))
   (is (= [["a","b","c","d"]]
-	 (binding [*strict* false] (parse-csv "a,b,c,\"d"))))
+         (binding [*strict* false] (parse-csv "a,b,c,\"d"))))
   (is (= [["a","b","c","d"]]
-	 (binding [*strict* true] (parse-csv "a,b,c,\"d\""))))
+         (binding [*strict* true] (parse-csv "a,b,c,\"d\""))))
   (is (= [["a","b","c","d\""]]
-	   (binding [*strict* false] (parse-csv "a,b,c,d\""))))
+           (binding [*strict* false] (parse-csv "a,b,c,d\""))))
   (is (= [["120030" "BLACK COD FILET MET VEL \"MSC\"" "KG" "0" "1"]]
        (binding [*strict* false *delimiter* \;]
-	 (parse-csv "120030;BLACK COD FILET MET VEL \"MSC\";KG;0;1")))))
+         (parse-csv "120030;BLACK COD FILET MET VEL \"MSC\";KG;0;1")))))
