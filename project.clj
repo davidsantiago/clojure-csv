ae636ddb8f5b54999a063274f45962a8bd209499
David Santiago <david.santiago@gmail.com>
2011-01-27 19:33:13 +0100

    Untabify project.clj.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clojure-csv "1.2.2"
   :description "A simple library to read and write CSV files."
   :dependencies [[org.clojure/clojure "1.2.0"]
-		 [org.clojure/clojure-contrib "1.2.0"]]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
