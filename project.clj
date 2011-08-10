05496a8e9adfcaa94543d4cc4e354e3b870f9c60
David Santiago <david.santiago@gmail.com>
2011-08-10 15:02:38 -0500

    Change version to 1.3.1 for release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-csv "1.3.1-SNAPSHOT"
+(defproject clojure-csv "1.3.1"
   :description "A simple library to read and write CSV files."
   :dependencies [[org.clojure/clojure "1.3.0-beta1"]]
   :dev-dependencies [[swank-clojure "1.3.0"
