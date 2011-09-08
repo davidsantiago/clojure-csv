1ccaadc0916e19bf7faf7fd825c65d60c1e48193
David Santiago <david.santiago@gmail.com>
2011-09-08 12:39:45 -0500

    Update to version to 2.0.0-SNAPSHOT and Clojure 1.3.0-beta3.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clojure-csv "1.3.2"
+(defproject clojure-csv "2.0.0-SNAPSHOT"
   :description "A simple library to read and write CSV files."
-  :dependencies [[org.clojure/clojure "1.3.0-beta1"]]
+  :dependencies [[org.clojure/clojure "1.3.0-beta3"]]
   :dev-dependencies [[swank-clojure "1.3.0"
                       :exclusions [org.clojure/clojure
                                    org.clojure/clojure-contrib]]])
