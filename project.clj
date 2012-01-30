74a865c76b9b54ba5794accdf465605bbb1cd45d
David Santiago <david.santiago@gmail.com>
2012-01-29 19:18:34 -0600

    Update project to swank-clojure 1.4.0.

--- project.clj
+++ project.clj
@@ -1,7 +1,5 @@
 (defproject clojure-csv "2.0.0-SNAPSHOT"
   :description "A simple library to read and write CSV files."
   :dependencies [[org.clojure/clojure "1.3.0"]]
-  :dev-dependencies [[swank-clojure "1.3.0"
-                      :exclusions [org.clojure/clojure
-                                   org.clojure/clojure-contrib]]]
+  :dev-dependencies [[swank-clojure "1.4.0"]]
   :jvm-opts ["-Xmx1g"])
