f185a8b66c1c043d572fa0dc9cb3673fd74b4de9
David Santiago <david.santiago@gmail.com>
2012-02-21 00:49:25 -0800

    Update project.clj to version 2.0.0-alpha1.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-csv "2.0.0-SNAPSHOT"
+(defproject clojure-csv "2.0.0-alpha1"
   :description "A simple library to read and write CSV files."
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :dev-dependencies [[swank-clojure "1.4.0"]]
