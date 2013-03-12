be8c586bf0f8f79bfef48496a0da8f7f07911ee5
Barrie McGuire <barriemcguire@bcm-rmbp.(none)>
2013-03-12 21:01:54 +0000

    Create reproducible version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-csv "2.0.0-alpha3-SNAPSHOT"
+(defproject clojure-csv "2.0.0-alpha3-20120312"
   :description "A simple library to read and write CSV files."
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :plugins [[perforate "0.2.0"]]
