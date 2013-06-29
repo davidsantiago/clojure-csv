b0255d4775c9fd4e504e2356cf185ecdd69d2eba
supersymmetry <maildumpert@gmail.com>
2013-06-29 21:04:14 +0200

    updated dependencies/clj1.5.1

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject clojure-csv "2.0.0-alpha2"
   :description "A simple library to read and write CSV files."
-  :dependencies [[org.clojure/clojure "1.3.0"]]
-  :plugins [[perforate "0.2.0-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :plugins [[perforate "0.3.2"]]
   :jvm-opts ["-Xmx1g"]
   :profiles {:current {:source-paths ["src/"]}
              :clj1.4 {:dependencies [[org.clojure/clojure "1.4.0-beta5"]]}
              :clj1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :csv1.3 {:dependencies [[clojure-csv "1.3.0"]]}
+             :csv1.3 {:dependencies [[clojure-csv "1.3.2"]]}
              :csv2.0 {:dependencies [[clojure-csv "2.0.0-alpha1"]]}}
   :perforate {:environments [{:name :clojure-csv2
                               :profiles [:clj1.3 :csv2.0]
