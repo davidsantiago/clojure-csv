fd30902d229089e59b2fbae62cbc24e87f7267c5
David Santiago <david.santiago@gmail.com>
2016-01-06 22:22:56 -0800

    Update version for further development.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-csv "2.0.2"
+(defproject clojure-csv "2.0.3-SNAPSHOT"
   :description "A simple library to read and write CSV files."
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :plugins [[perforate "0.3.2"]]
