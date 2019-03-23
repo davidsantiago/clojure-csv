129b11d26ac0d61e3adec55bc5e50ea43dc7647e
Simon Brooke <simon@journeyman.cc>
2019-03-23 13:22:32 +0000

    Uncommented the bits I'd commented out of the project file

--- project.clj
+++ project.clj
@@ -3,19 +3,19 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-time "0.15.0"]]
   :plugins [[perforate "0.3.2"]]
-  :jvm-opts ["-Xmx1g"])
-;;   :profiles {:current {:source-paths ["src/"]}
-;;              :clj1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
-;;              :clj1.4 {:dependencies [[org.clojure/clojure "1.4.0-beta5"]]}
-;;              :clj1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-;;              :csv1.3 {:dependencies [[clojure-csv "1.3.0"]]}
-;;              :csv2.0 {:dependencies [[clojure-csv "2.0.0-alpha1"]]}}
-;;   :perforate {:environments [{:name :clojure-csv2
-;;                               :profiles [:clj1.3 :csv2.0]
-;;                               :namespaces [csv.benchmarks.core]}
-;;                              {:name :clojure-csv1
-;;                               :profiles [:clj1.3 :csv1.3]
-;;                               :namespaces [csv.benchmarks.core]}
-;;                              {:name :current
-;;                               :profiles [:clj1.8 :current]
-;;                               :namespaces [csv.benchmarks.core]}]})
+  :jvm-opts ["-Xmx1g"]
+  :profiles {:current {:source-paths ["src/"]}
+             :clj1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :clj1.4 {:dependencies [[org.clojure/clojure "1.4.0-beta5"]]}
+             :clj1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :csv1.3 {:dependencies [[clojure-csv "1.3.0"]]}
+             :csv2.0 {:dependencies [[clojure-csv "2.0.0-alpha1"]]}}
+  :perforate {:environments [{:name :clojure-csv2
+                              :profiles [:clj1.3 :csv2.0]
+                              :namespaces [csv.benchmarks.core]}
+                             {:name :clojure-csv1
+                              :profiles [:clj1.3 :csv1.3]
+                              :namespaces [csv.benchmarks.core]}
+                             {:name :current
+                              :profiles [:clj1.8 :current]
+                              :namespaces [csv.benchmarks.core]}]})
